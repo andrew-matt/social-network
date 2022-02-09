134bc8c4eb606cb6fb19dea7a08e6e1e08d7aca4
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-09 11:30:04 +0300

    Update Dialogs.tsx

--- components/Dialogs/Dialogs.tsx
+++ components/Dialogs/Dialogs.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Andrew
+                </div>
+                <div className={s.dialog}>
+                    Hannah
+                </div>
+                <div className={s.dialog}>
+                    William
+                </div>
+                <div className={s.dialog}>
+                    Jennifer
+                </div>
+                <div className={s.dialog}>
+                    Holly
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>Hell yeah</div>
+            </div>
         </div>
     )
 }
