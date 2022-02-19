8355b866139b7dfef5d5bc979e9de686068bd407
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-19 15:31:24 +0300

    dialogs

--- components/Dialogs/Dialogs.tsx
+++ components/Dialogs/Dialogs.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {DialogItem} from "./DialogItem/DialogItem";
-import {Message} from './Message/Message';
+import {DialogItem, DialogItemPropsType} from "./DialogItem/DialogItem";
+import {Message, MessagePropsType} from './Message/Message';
 
-export const Dialogs = () => {
+export type DialogsPropsType = {
+    dialogs: Array<DialogItemPropsType>
+    messages: Array<MessagePropsType>
+}
 
-    let dialogs = [
-        {id: 1, name: "Andrew"},
-        {id: 2, name: "Hannah"},
-        {id: 3, name: "William"},
-        {id: 4, name: "Jennifer"},
-        {id: 5, name: "Holly"}
-    ]
+export const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
-    let message = [
-        {id: 1, message: "Hi"},
-        {id: 2, message: "How are you"},
-        {id: 3, message: "Hell yeah"}
-    ]
-
-    let dialogElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messageElements = message.map(m => <Message message={m.message}/>);
+    let dialogElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messageElements = props.messages.map(m => <Message message={m.message}/>);
 
     return (
         <div className={s.dialogs}>
