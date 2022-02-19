a64bba11ccc7e0c7dd14216ec5547385bffe8687
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-19 15:30:43 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
-import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Profile, ProfilePropsType} from './components/Profile/Profile';
+import {Dialogs, DialogsPropsType} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 
+type AppPropsType = ProfilePropsType & DialogsPropsType
 
-const App = () => {
+const App: React.FC<AppPropsType> = (props) => {
+
     return (
         <BrowserRouter>
         <div className='app-wrapper'>
@@ -18,8 +20,8 @@ const App = () => {
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Routes>
-                <Route path='/profile' element={<Profile/>}/>
-                <Route path='/dialogs' element={<Dialogs/>}/>
+                <Route path='/profile' element={<Profile posts={props.posts}/>}/>
+                <Route path='/dialogs' element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
                 <Route path='/news' element={<News/>}/>
                 <Route path='/music' element={<Music/>}/>
                 <Route path='/settings' element={<Settings/>}/>
