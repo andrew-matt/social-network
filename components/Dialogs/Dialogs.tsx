423ddb29e36bc07e293338ed0bdad6e52f196f5a
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-20 11:54:52 +0300

    Update Dialogs.tsx

--- components/Dialogs/Dialogs.tsx
+++ components/Dialogs/Dialogs.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import {DialogItem, DialogItemPropsType} from "./DialogItem/DialogItem";
-import {Message, MessagePropsType} from './Message/Message';
+import {DialogItem} from "./DialogItem/DialogItem";
+import {Message,} from './Message/Message';
+import {DialogPageType} from "../../Redux/State";
 
 export type DialogsPropsType = {
-    dialogs: Array<DialogItemPropsType>
-    messages: Array<MessagePropsType>
+    state: DialogPageType
 }
 
 export const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
-    let dialogElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messageElements = props.messages.map(m => <Message message={m.message}/>);
+    let dialogElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messageElements = props.state.messages.map(m => <Message message={m.message}/>);
 
     return (
         <div className={s.dialogs}>
