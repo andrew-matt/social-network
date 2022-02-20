66364f90af3e7b10cf7cf966bf97ec309ed39903
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-20 11:54:05 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import {Profile, ProfilePropsType} from './components/Profile/Profile';
-import {Dialogs, DialogsPropsType} from "./components/Dialogs/Dialogs";
+import {Profile} from './components/Profile/Profile';
+import {Dialogs} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
+import {RootStateType} from "./Redux/State";
 
-type AppPropsType = ProfilePropsType & DialogsPropsType
+export type AppPropsType = {
+    state: RootStateType
+}
 
 const App: React.FC<AppPropsType> = (props) => {
 
@@ -20,8 +23,8 @@ const App: React.FC<AppPropsType> = (props) => {
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Routes>
-                <Route path='/profile' element={<Profile posts={props.posts}/>}/>
-                <Route path='/dialogs' element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
+                <Route path='/profile' element={<Profile state={props.state.profilePage}/>}/>
+                <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage}/>}/>
                 <Route path='/news' element={<News/>}/>
                 <Route path='/music' element={<Music/>}/>
                 <Route path='/settings' element={<Settings/>}/>
