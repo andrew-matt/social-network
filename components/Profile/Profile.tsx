ec0572e3a676fd33617a61ab7c4b22fce0ff9428
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-20 11:56:06 +0300

    Update Profile.tsx

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import MyPosts, {MyPostsPropsType} from "./MyPosts/MyPosts";
+import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {ProfilePageType} from "../../Redux/State";
 
-export type ProfilePropsType = MyPostsPropsType
+export type ProfilePropsType = {
+    state: ProfilePageType
+}
 
 export const Profile: React.FC<ProfilePropsType> = (props) => {
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts posts={props.posts}/>
+            <MyPosts posts={props.state.posts}/>
         </div>
     )
 }
