7e92a047d585513783f8ede885f280698c7a79b9
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-20 11:55:19 +0300

    Update DialogItem.tsx

--- components/Dialogs/DialogItem/DialogItem.tsx
+++ components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from '../Dialogs.module.css';
+import {DialogType} from "../../../Redux/State";
 
-export type DialogItemPropsType = {
-    name: string
-    id: number
-}
+export type DialogItemPropsType = DialogType
 
 export const DialogItem: React.FC<DialogItemPropsType> = (props) => {
     let path = '/dialogs/' + props.id;
