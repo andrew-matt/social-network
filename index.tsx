3a903053b5a9754fbd2266a97a992d176ef56878
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-20 11:54:13 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,29 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let posts = [
-    {id: 1, message: "Hi, how are you?", likesCount: 15},
-    {id: 2, message: "It's my first post!", likesCount: 20}
-]
-
-let dialogs = [
-    {id: 1, name: "Andrew"},
-    {id: 2, name: "Hannah"},
-    {id: 3, name: "William"},
-    {id: 4, name: "Jennifer"},
-    {id: 5, name: "Holly"}
-]
-
-let messages = [
-    {id: 1, message: "Hi"},
-    {id: 2, message: "How are you"},
-    {id: 3, message: "Hell yeah"}
-]
+import state from "./Redux/State";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
