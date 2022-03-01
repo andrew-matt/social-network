94168f8298fb87cc8b485aafd2cebede159ebb11
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-03-01 16:40:54 +0300

    Update Dialogs.tsx

--- components/Dialogs/Dialogs.tsx
+++ components/Dialogs/Dialogs.tsx
@@ -12,6 +12,12 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
     let dialogElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messageElements = props.state.messages.map(m => <Message message={m.message}/>);
+    
+    let newMessageElement = React.createRef<HTMLTextAreaElement>()
+
+    const addMessage = () => {
+        alert(newMessageElement.current?.value)
+    }
 
     return (
         <div className={s.dialogs}>
@@ -20,6 +26,8 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
             </div>
             <div className={s.messages}>
                 {messageElements}
+                <div><textarea ref={newMessageElement}></textarea></div>
+                <div><button onClick={addMessage}>Add message</button></div>
             </div>
         </div>
     )
