91f19266dfe458b3b97d3198a937b5030f2a44da
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-03-04 16:46:08 +0300

    Update Dialogs.tsx

--- components/Dialogs/Dialogs.tsx
+++ components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css';
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message,} from './Message/Message';
@@ -15,17 +15,12 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
     let dialogElements = props.state.dialogs.map(d => <DialogItem key={d.id} name={d.name} id={d.id}/>);
     let messageElements = props.state.messages.map(m => <Message key={m.id} message={m.message}/>);
 
-    let newMessageElement = React.createRef<HTMLTextAreaElement>()
-
     const addMessage = () => {
        props.addMessage()
     }
 
-    const onMessageChange = () => {
-        let text = newMessageElement.current?.value
-        if (text) {
-            props.updateNewMessageText(text)
-        }
+    const onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        props.updateNewMessageText(e.currentTarget.value)
     }
 
     return (
@@ -35,7 +30,7 @@ export const Dialogs: React.FC<DialogsPropsType> = (props) => {
             </div>
             <div className={s.messages}>
                 {messageElements}
-                <div><textarea onChange={onMessageChange} ref={newMessageElement} value={props.state.newMessageText}></textarea></div>
+                <div><textarea onChange={onMessageChange} value={props.state.newMessageText}></textarea></div>
                 <div><button onClick={addMessage}>Add message</button></div>
             </div>
         </div>
