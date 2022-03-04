e75e0c37d6b75e55e9016c54db0526323addfb24
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-03-04 18:37:18 +0300

    Update render.tsx

--- render.tsx
+++ render.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, RootStateType} from "./Redux/State";
+import {addMessage, addPost, RootStateType, updateNewMessageText, updateNewPostText} from "./Redux/State";
 
 export function rerenderEntireTree(state: RootStateType) {
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} addPost={addPost}/>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText}/>
         </React.StrictMode>,
         document.getElementById('root')
     );
