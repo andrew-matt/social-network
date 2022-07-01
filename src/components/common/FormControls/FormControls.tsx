b0eda357a03c034fe9f0ea92359e13af5920df22
andrew-matt <andrew.matt@ya.ru>
2022-07-01 15:22:19 +0300

    add FormControl component

--- src/components/common/FormControls/FormControls.tsx
+++ src/components/common/FormControls/FormControls.tsx
@@ -1,25 +1,23 @@
 import styles from './FormsControls.module.css'
 
-export const Textarea = (props: any) => {
+export const FormControl = (props: any) => {
 
     const showError = props.meta.touched && props.meta.error
 
     return (
         <div className={styles.formControl + ' ' + (showError && styles.error)}>
-            <div><textarea {...props.input}/></div>
+            <div>
+                {props.children}
+            </div>
             {showError && <span className={styles.error}>some error</span>}
         </div>
     );
 };
 
+export const Textarea = (props: any) => {
+    return <FormControl {...props}><textarea {...props.input}/></FormControl>
+};
+
 export const Input = (props: any) => {
-
-    const showError = props.meta.touched && props.meta.error
-
-    return (
-        <div className={styles.formControl + ' ' + (showError && styles.error)}>
-            <div><input {...props.input} placeholder={props.placeholder}/></div>
-            {showError && <span className={styles.error}>some error</span>}
-        </div>
-    );
+    return <FormControl {...props}><input {...props.input} placeholder={props.placeholder}/></FormControl>
 };
