8f433fced5b6dd3333eecc24cb3cd938c4c24e87
andrew-matt <andrew.matt@ya.ru>
2022-08-22 19:06:10 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import store from './Redux/Redux-Store';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {HashRouter} from 'react-router-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <HashRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </HashRouter>
-    </React.StrictMode>,
-    document.getElementById('root'),
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement,
+);
+root.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
