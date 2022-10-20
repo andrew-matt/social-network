5e7e3e5e44ccab6d425ac11d85c9d3d1d378a223
andrew-matt <andrew.matt@ya.ru>
2022-10-20 11:14:04 +0300

    update navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from 'components/Navbar/Navbar'
+import {Navbar} from 'components/Navbar/Navbar'
 import {Navigate, Route, Routes} from 'react-router-dom'
 import {News} from 'components/News/News'
 import {Music} from 'components/Music/Music'
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,51 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
-import s from './Navbar.module.css';
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import style from './Navbar.module.css'
+import {
+    faUser,
+    faComment,
+    faUserGroup,
+    faNewspaper,
+    faMusic,
+    faGear,
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <NavLink to='/profile' className={navData => navData.isActive ? s.activeLink : s.item}>Profile</NavLink>
+export const Navbar = () => {
+    return (
+        <div className={style.block}>
+            <nav className={style.nav}>
+                <NavLink to="/profile"
+                         className={navData => `${style.item} ${navData.isActive && style.activeLink}`}>
+                    <FontAwesomeIcon icon={faUser}/>
+                    <span>Profile</span>
+                </NavLink>
+                <NavLink to="/dialogs"
+                         className={navData => `${style.item} ${navData.isActive && style.activeLink}`}>
+                    <FontAwesomeIcon icon={faComment}/>
+                    <span>Messages</span>
+                </NavLink>
+                <NavLink to="/users"
+                         className={navData => `${style.item} ${navData.isActive && style.activeLink}`}>
+                    <FontAwesomeIcon icon={faUserGroup}/>
+                    <span>Users</span>
+                </NavLink>
+                <NavLink to="/news"
+                         className={navData => `${style.item} ${navData.isActive && style.activeLink}`}>
+                    <FontAwesomeIcon icon={faNewspaper}/>
+                    <span>News</span>
+                </NavLink>
+                <NavLink to="/music"
+                         className={navData => `${style.item} ${navData.isActive && style.activeLink}`}>
+                    <FontAwesomeIcon icon={faMusic}/>
+                    <span>Music</span>
+                </NavLink>
+                <NavLink to="/settings"
+                         className={navData => `${style.item} ${navData.isActive && style.activeLink}`}>
+                    <FontAwesomeIcon icon={faGear}/>
+                    <span>Settings</span>
+                </NavLink>
+            </nav>
         </div>
-        <div className={s.item}>
-            <NavLink to='/dialogs' className={navData => navData.isActive ? s.activeLink : s.item}>Messages</NavLink>
-        </div>
-        <div className={s.item}>
-            <NavLink to='/users' className={navData => navData.isActive ? s.activeLink : s.item}>Users</NavLink>
-        </div>
-        <div className={s.item}>
-            <NavLink to='/news' className={navData => navData.isActive ? s.activeLink : s.item}>News</NavLink>
-        </div>
-        <div className={s.item}>
-            <NavLink to='/music' className={navData => navData.isActive ? s.activeLink : s.item}>Music</NavLink>
-        </div>
-        <div className={s.item}>
-            <NavLink to='/settings' className={navData => navData.isActive ? s.activeLink : s.item}>Settings</NavLink>
-        </div>
-    </nav>
+    )
 }
-
-export default Navbar;
